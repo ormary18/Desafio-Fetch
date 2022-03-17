88bc8a255983464c4d5bf231c2da41a4ba494c24
Ormary Montilla <ormary.montilla@gmail.com>
2022-03-16 23:24:43 -0300

    Desafio agregando Fetch

---
+++ Js/base.js
@@ -0,0 +1,28 @@
+
+const tabla = document.querySelector('#nomina');
+
+function cargarusarios(){
+    fetch('nomina.json')
+    .then(respuesta => respuesta.json())
+    .then(usuarios => {
+        usuarios.forEach(usuario => {
+        const row = document.createElement('tr');
+        row.innerHTML += `
+        <td>${usuario.id}</td>
+        <td>${usuario.nombre}</td>
+        <td>${usuario.Apellido}</td>
+        <td>${usuario.direccion}</td>
+        <td>${usuario.telefono}</td>
+        <td>${usuario.puesto}</td>
+    
+        `;
+tabla.appendChild(row);
+            
+        });
+        
+})
+.catch(error => console.log('Hubo un error: ' + error.message))
+    
+    }
+
+cargarusarios();
